a3e78795bc89a41d9b13787293fb5bb3434f3cc6
Gigiiase <gigiiaseshvili94@gmail.com>
2025-01-26 16:15:00 +0400

    ggg

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-// // Sample URL for fetching the data (replace with your actual API URL)
-// const apiUrl = "https://api.example.com/artist";  // Replace with the actual API endpoint
+ const musicSection = document.getElementById('main-content');
+const apiUrl = "https://api.example.com/artist";  
+fetch(apiUrl)
+  .then(response => response.json()) 
+  .then(data => {
+    const artistName = data.name;
+    const artistGenres = data.genres.join(", "); 
+    const artistImage = data.images[0].url;
+    const artistPopularity = data.popularity;
+    const artistFollowers = data.followers.total;
 
-// // Fetch data from the API
-// fetch(apiUrl)
-//   .then(response => response.json()) // Convert the response into JSON
-//   .then(data => {
-//     // Extracting relevant data from the API response
-//     const artistName = data.name;
-//     const artistGenres = data.genres.join(", "); // Joining genres into a string
-//     const artistImage = data.images[0].url;
-//     const artistPopularity = data.popularity;
-//     const artistFollowers = data.followers.total;
+    document.getElementById('artist-name').textContent = artistName;
+    document.getElementById('artist-genres').textContent = artistGenres;
+    document.getElementById('artist-image').src = artistImage;
+    document.getElementById('artist-popularity').textContent = `Popularity: ${artistPopularity}`;
+    document.getElementById('artist-followers').textContent = `Followers: ${artistFollowers}`;
+  })
+  .catch(error => {
+    console.error("Error fetching data: ", error);
+  });
 
-//     // Use the extracted data to dynamically update your HTML
-//     document.getElementById('artist-name').textContent = artistName;
-//     document.getElementById('artist-genres').textContent = artistGenres;
-//     document.getElementById('artist-image').src = artistImage;
-//     document.getElementById('artist-popularity').textContent = `Popularity: ${artistPopularity}`;
-//     document.getElementById('artist-followers').textContent = `Followers: ${artistFollowers}`;
-//   })
-//   .catch(error => {
-//     console.error("Error fetching data: ", error);
-//   });
+  artist.forEach(musicList => {
+    const div = document.createElement('div');
+    div.className = 'music list';
+    div.innerHTML = `   <div class="artist-container">
+        <img id="${artistImage}" src="" alt="Artist Image" class="artist-image">
+        <h2 id="artist-name">${artistName}</h2>
+        <p id="artist-genres">${artistGenres}</p>
+        <p id="artist-popularity">Popularity; ${artistPopularity}</p>
+        <p id="artist-followers">Followers: ${artistFollowers}</p>
+    </div>`;
+    musicSection.appendChild('div');
+  });
